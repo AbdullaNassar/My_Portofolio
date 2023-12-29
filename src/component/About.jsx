40e3136f9ac56395a396d77da8b5de5de6c1159d
abdo <abdallah moemen44@gmail.com>
2023-12-30 01:12:59 +0200

    update about

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -5,7 +5,7 @@ import { FaSquareFacebook } from "react-icons/fa6";
 import { IoGitCommitOutline } from "react-icons/io5";
 function About() {
   return (
-    <div className="h-screen">
+    <div className="min-h-screen">
       <h1 className="relative mx-auto mt-8 w-fit border-b-2 border-b-yellow text-center  font-bold uppercase tracking-[5px] opacity-70 md:text-4xl">
         <span className="block pb-2 ">About me</span>
         <div className=" absolute left-[50%] top-[44px] h-[10px] w-[10px] translate-x-[-50%] rounded-[50%] bg-yellow"></div>
