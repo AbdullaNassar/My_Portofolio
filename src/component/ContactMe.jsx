5c8e3e0247e2258f9b6774c7306368039d4597ba
abdo <abdallah moemen44@gmail.com>
2023-12-31 23:27:56 +0200

    add contact form handle

--- src/component/ContactMe.jsx
+++ src/component/ContactMe.jsx
@@ -1,5 +1,34 @@
+import toast from "react-hot-toast";
 import contact2 from "../images/contact2.png";
+import { useState } from "react";
 function ContactMe() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (name === "") {
+      toast.error("Enter your name");
+      return;
+    }
+    if (message === "") {
+      toast.error("Enter your message");
+      return;
+    }
+    if (name.length < 5) {
+      toast.error("please enter at least 5 characters in your name");
+      return;
+    }
+
+    if (message.length < 5) {
+      toast.error("please enter at least 5 characters in your message");
+      return;
+    }
+    toast.success("Your message has been sent succusfuly");
+    setName("");
+    setEmail("");
+    setMessage("");
+  }
   return (
     <div className=" mt-20 flex flex-col justify-center gap-16 px-4 sm:px-24 md:flex-row">
       <div className="flex flex-col">
@@ -13,13 +42,15 @@ function ContactMe() {
           alt="Contact Picture"
         />
       </div>
-      <form className="flex flex-col gap-2">
+      <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-4 md:flex-row">
           <div className="flex flex-col gap-1">
             <label className="font-medium" htmlFor="name">
               Your name
             </label>
             <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               className="min-h-10 rounded-md bg-primaryLight px-2 py-1 text-neutral-300 focus:outline-none focus:ring-1 focus:ring-yellow focus:ring-offset-2"
               id="name"
               placeholder="Name"
@@ -30,6 +61,9 @@ function ContactMe() {
               Your Email
             </label>
             <input
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="min-h-10 rounded-md bg-primaryLight px-2 py-1 text-neutral-300 focus:outline-none focus:ring-1 focus:ring-yellow focus:ring-offset-2"
               id="email"
               type="email"
@@ -42,6 +76,8 @@ function ContactMe() {
             Your Message
           </label>
           <textarea
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
             className="min-h-28 flex-grow rounded-md  bg-primaryLight px-2 py-1 text-neutral-300 focus:outline-none focus:ring-1 focus:ring-yellow focus:ring-offset-2"
             placeholder="Message"
           />
