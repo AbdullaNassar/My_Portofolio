eed37b2f397fb80609cd85f93e4200d0f2421ab1
abdo <abdallah moemen44@gmail.com>
2023-12-31 11:08:21 +0200

    update skills sction

--- src/component/SkillItem.jsx
+++ src/component/SkillItem.jsx
@@ -1,11 +1,13 @@
 function SkillItem({ name, srcImage }) {
   return (
-    <div className="bg-primary2 flex w-[350px] items-center justify-between justify-self-center rounded-lg px-4 py-5">
-      <div className="w-[50px]">
+    <div className="flex w-[350px] items-center justify-between justify-self-center rounded-lg bg-primary2 px-4 py-5">
+      <div className="w-[40px] sm:w-[50px]">
         {" "}
         <img className="w-[100%]" src={srcImage} alt={name} />
       </div>
-      <span className="text-2xl font-semibold text-yellow">{name}</span>
+      <span className="text-lg font-semibold text-yellow sm:text-2xl">
+        {name}
+      </span>
     </div>
   );
 }
