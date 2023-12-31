81b38cc1661e794312fdfb3be1d2584d03ce71f9
abdo <abdallah moemen44@gmail.com>
2023-12-31 11:04:16 +0200

    test heading change

--- src/component/Heading.jsx
+++ src/component/Heading.jsx
@@ -1,8 +1,8 @@
 function Heading({ children }) {
   return (
-    <h1 className="text2xl relative mx-auto mt-8 w-fit border-b-2 border-b-yellow  text-center font-bold uppercase tracking-[5px] opacity-70 sm:text-2xl md:text-4xl">
+    <h1 className="relative mx-auto mt-8 w-fit border-b-2 border-b-yellow text-center  text-2xl font-bold uppercase tracking-[5px] opacity-70 sm:text-3xl md:text-4xl">
       <span className="block pb-2 ">{children}</span>
-      <div className=" absolute left-[50%] top-[28px] top-[36px] h-[10px] w-[10px] translate-x-[-50%] rounded-[50%] bg-yellow md:top-[44px]"></div>
+      <div className=" absolute left-[50%]  top-[36px] h-[10px] w-[10px] translate-x-[-50%] rounded-[50%] bg-yellow sm:top-[40px] md:top-[44px]"></div>
       {/* <hr className="text-yellow" /> */}
     </h1>
   );
