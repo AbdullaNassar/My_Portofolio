cae2a788977dc748c7694f82c4a6ac11f9e5dc4a
abdo <abdallah moemen44@gmail.com>
2023-12-31 07:46:36 +0200

    Add my projects

--- src/component/ProjectItem.jsx
+++ src/component/ProjectItem.jsx
@@ -1,25 +1,34 @@
-function ProjectItem({ srcImage, title, description, tags = [] }) {
+function ProjectItem({ srcImage, title, url, description, tags = [] }) {
   return (
-    <div className=" bg-primary2 flex w-[250px] flex-col justify-self-center  overflow-hidden rounded-md lg:w-[350px]">
+    <div className=" flex w-[250px] flex-col justify-self-center overflow-hidden  rounded-md bg-primary2 lg:w-[350px]">
       <img className="h-[200px] lg:h-[200px]" src={srcImage} alt="project1" />
-      <h2 className="text-l mb-2 mt-2 pl-2 font-semibold text-yellow md:text-xl">
-        {title}
-      </h2>
-      <p className="mb-4 pl-2">{description}</p>
-      <div className="flex flex-wrap gap-1 pl-2">
-        {tags.map((tag) => (
-          <div
-            key={Math.random()}
-            className="flex w-fit items-center gap-2 rounded-full bg-neutral-200 px-2 py-1  text-sm font-semibold text-gray-800"
-          >
-            <img className="h-6 w-6" src={tag.tagSrc} />
-            <span>{tag.tagName}</span>
+      <div className="flex flex-grow flex-col justify-between">
+        <div>
+          <h2 className="text-l mb-2 mt-2 pl-2 font-semibold text-yellow md:text-xl">
+            {title}
+          </h2>
+          <p className="mb-4 pl-2">{description}</p>
+          <div className="flex flex-wrap gap-2 pl-2">
+            {tags.map((tag) => (
+              <div
+                key={Math.random()}
+                className="flex w-fit items-center gap-2 rounded-full bg-neutral-200 px-2 py-1  text-sm font-semibold text-gray-800"
+              >
+                <img className="h-6 w-6" src={tag.tagSrc} />
+                <span>{tag.tagName}</span>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
+        <a
+          href={url}
+          target="_blank"
+          rel="noreferrer"
+          className="mb-4 mt-4 self-center rounded-md bg-yellow px-4 py-3 pl-2 text-center font-semibold text-gray-900"
+        >
+          Go to {title} &rarr;
+        </a>
       </div>
-      <button className="mb-4 mt-4 self-center rounded-md bg-yellow px-4 py-3 pl-2 text-center font-semibold text-gray-900">
-        Go to {title} &rarr;
-      </button>
     </div>
   );
 }
--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -1,32 +1,108 @@
 import Heading from "./Heading";
 import ProjectItem from "./ProjectItem";
 import screen1 from "../images/screentemp.png";
+import supabase from "../images/supabase.png";
+import router from "../images/router.png";
+import query from "../images/query.png";
+import hookform from "../images/hookform.png";
+import proj2 from "../images/proj2.png";
+import proj3 from "../images/proj3.png";
+import proj4 from "../images/proj4.png";
 const projectsList = [
   {
     title: "Clinic",
     srcImage: screen1,
+    url: "https://abdullah-clinic.netlify.app/",
     description:
       "Full website for managing personal clinic operations including booking, patient information, inventory and financial tracking.",
     tags: [
       {
-        tagName: "HTML",
-        tagSrc: screen1,
+        tagName: "React.js",
+        tagSrc:
+          "https://firebasestorage.googleapis.com/v0/b/photo-gallery-4a3be.appspot.com/o/icons8-react-native-100.png?alt=media&token=5a314199-b540-449f-be6e-bd2e6a84a8ea",
+      },
+      {
+        tagName: "Supabase",
+        tagSrc: supabase,
       },
       {
         tagName: "CSS",
-        tagSrc: screen1,
+        tagSrc:
+          "https://firebasestorage.googleapis.com/v0/b/photo-gallery-4a3be.appspot.com/o/icons8-css3-100.png?alt=media&token=deebc249-7c87-4fb9-869f-5888591aaeac",
       },
+      {
+        tagName: "React Router ",
+        tagSrc: router,
+      },
+      {
+        tagName: "React Query",
+        tagSrc: query,
+      },
+      {
+        tagName: "React Hook Form",
+        tagSrc: hookform,
+      },
+    ],
+  },
+
+  {
+    title: "Neutrous",
+    srcImage: proj2,
+    url: "https://landing-page-abdo.netlify.app/",
+    description: "Responsive landing page built using HTML and SASS",
+    tags: [
+      {
+        tagName: "HTML",
+        tagSrc:
+          "https://firebasestorage.googleapis.com/v0/b/photo-gallery-4a3be.appspot.com/o/icons8-html-5-100.png?alt=media&token=2445495f-7bf3-4f39-a49e-219164cd67a4",
+      },
+      {
+        tagName: "Sass",
+        tagSrc:
+          "https://firebasestorage.googleapis.com/v0/b/photo-gallery-4a3be.appspot.com/o/icons8-css3-100.png?alt=media&token=deebc249-7c87-4fb9-869f-5888591aaeac",
+      },
+    ],
+  },
+
+  {
+    title: "Blog App",
+    srcImage: proj3,
+    url: "https://abdo-blog.netlify.app/",
+    description:
+      "A web page to add, search and delete blogs from your blog list. It is created using React.js and CSS modules",
+    tags: [
       {
         tagName: "Javascript",
-        tagSrc: screen1,
+        tagSrc:
+          "https://firebasestorage.googleapis.com/v0/b/photo-gallery-4a3be.appspot.com/o/icons8-javascript-100.png?alt=media&token=581e15c5-d48c-49e3-ae93-2cf75529edfa",
       },
       {
-        tagName: "HTML",
-        tagSrc: screen1,
+        tagName: "React.js",
+        tagSrc:
+          "https://firebasestorage.googleapis.com/v0/b/photo-gallery-4a3be.appspot.com/o/icons8-react-native-100.png?alt=media&token=5a314199-b540-449f-be6e-bd2e6a84a8ea",
       },
       {
+        tagName: "CSS",
+        tagSrc:
+          "https://firebasestorage.googleapis.com/v0/b/photo-gallery-4a3be.appspot.com/o/icons8-css3-100.png?alt=media&token=deebc249-7c87-4fb9-869f-5888591aaeac",
+      },
+    ],
+  },
+  {
+    title: "Nexter",
+    srcImage: proj4,
+    url: "https://abdo-nexter.netlify.app/",
+    description: "Responsive Template page",
+    tags: [
+      {
         tagName: "HTML",
-        tagSrc: screen1,
+        tagSrc:
+          "https://firebasestorage.googleapis.com/v0/b/photo-gallery-4a3be.appspot.com/o/icons8-html-5-100.png?alt=media&token=2445495f-7bf3-4f39-a49e-219164cd67a4",
+      },
+      {
+        tagName: "Sass",
+        tagSrc:
+          "https://firebasestorage.googleapis.com/v0/b/photo-gallery-4a3be.appspot.com/o/icons8-css3-100.png?alt=media&token=deebc249-7c87-4fb9-869f-5888591aaeac",
       },
     ],
   },
@@ -35,13 +111,14 @@ function Projects() {
   return (
     <div className="min-h-screen">
       <Heading>Projects</Heading>
-      <div className="grid-cols-project mt-20 grid gap-y-8">
+      <div className="mt-20 grid grid-cols-project gap-y-8">
         {projectsList.map((project) => (
           <ProjectItem
             title={project.title}
             description={project.description}
             srcImage={project.srcImage}
             tags={project.tags}
+            url={project.url}
             key={Math.random()}
           />
         ))}
